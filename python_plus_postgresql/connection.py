682f19505dac13e0c80d01cba7f4967685aeb8bf
tseOrlando <20sw@GMAIL.COM>
2024-02-01 19:52:05 +0100

    create_table

--- python_plus_postgresql/connection.py
+++ python_plus_postgresql/connection.py
@@ -1,3 +1,6 @@
 import psycopg2
 
-conn = psycopg2.connect()
+# host sql peer and the satement executor
+
+peer = psycopg2.connect(database="pgdb", user="pgdb_user", password="pgdb_pass", host="localhost", port="5432")
+send = peer.cursor()
---
+++ python_plus_postgresql/create_table.py
@@ -0,0 +1,9 @@
+from connection import *
+
+def create_table():
+
+    if (input("do you want to create 'users' table? y/n\n") == 'y'):
+        send.execute("""CREATE TABLE users(user_id SERIAL PRIMARY KEY, user_name VARCHAR(255) NOT NULL, 
+                        password VARCHAR(255) NOT NULL, user_age BIGINT NOT NULL, gender VARCHAR(1) NOT NULL, 
+                        range BIGINT NOT NULL);""")
+        peer.commit()
